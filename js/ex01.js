bbd107003ccb4b298e1c787baad5065a615c1fb0
buba <f6774411@gmail.com>
2021-04-15 23:48:56 -0300

    Adicionei html

---
+++ js/ex01.js
@@ -0,0 +1,103 @@
+
+function clicou (){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+   // console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar");
+}
+
+/* Estrutura de repetição
+
+function soma (n1, n2,) {
+    return n1 + n2;
+}
+
+X
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual é a sua idade");
+console.log(validaIdade(idade))
+
+//alert(soma(5, 10));
+
+*/
+
+
+/*
+var d = new Date();
+alert(d.getMinutes())
+alert(d.getDay())
+alert(d.getHours)
+*/
+
+/* Estrutura de repetição
+
+var count;
+for(count=0; count <= 6; count++){
+    alert(count)
+}
+
+
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count ++;
+};
+*/
+
+
+/*
+var idade = prompt("Qual sua idade?")
+if (idade >= 18){
+    alert("maior de idade!")
+} else{
+    alert("menor de idade")
+}
+*/
+
+
+/*
+var fruta = [{nome: " maça ", cor: " vermelha"}, {nome: " uva ", cor: " rouxa"}]
+console.log(fruta);
+alert(fruta[1].nome);
+*/
+
+
+/*
+var fruta = {nome: " maça ", cor: " vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = [" maça ", " péra ", " laranja "];
+//lista.push(" Uva")
+//lista.pop();
+//console.log(lista);
+//console.log(lista.toString())
+//console.log(lista.join("-"))
+
+
+
+/* 
+
+var nome = "Filipe Buba";
+var n1 = 5;
+var n2 = 3;
+var frase = "Guiné-Bissau, é o melhior time do mundo";
+
+// alert(nome + " tem " + idade + " anos")
+// alert(idade + idade2)
+
+console.log(nome)
+console.log(n1 * n2)
+console.log(frase.toLowerCase());
+//alert(frase.replace("Guiné-Bissau", "Brasil"))
+*/
